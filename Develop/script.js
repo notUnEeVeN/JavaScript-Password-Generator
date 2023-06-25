75cd0c0316b2062b1003e39699f02e0306200423
notUnEeVeN <tybalt.mallet@gmail.com>
2023-06-25 13:18:29 -0700

    completed the first draft of the javascript code, the code is successfully generated

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,44 @@ function writePassword() {
 
   passwordText.value = password;
 
+   function generatePassword() {
+    var charset = "";
+
+    var password = "";
+
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var numbers = "0123456789";
+
+    var specialChar = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+    var input = prompt("How long do you want your password to be, select a number within the range of 8 to 128 characters.");
+    
+    var length = Number(input);
+
+    var lowerCaseQuestion = confirm("Do you want to include lowercase letters in your password?");
+    if(lowerCaseQuestion) charset += lowerCase;
+
+    var upperCaseQuestion = confirm("Do you want to include uppercase letters in your password?");
+    if(upperCaseQuestion) charset += upperCase;
+    
+    var numbersQuestion = confirm("Do you want to include numbers in your password?");
+    if(numbersQuestion) charset += numbers;
+
+    var specialCharQuestion = confirm("Do you want to include special characters in your password?");
+    if(specialCharQuestion) charset += specialChar;
+
+    for (let i = 0; i < length; i++) {
+      let index = Math.floor(Math.random() * charset.length);
+      password += charset[index];
+    }
+
+    alert("Your password is: " + password);
+
+    return password;
+}
 }
 
 // Add event listener to generate button
